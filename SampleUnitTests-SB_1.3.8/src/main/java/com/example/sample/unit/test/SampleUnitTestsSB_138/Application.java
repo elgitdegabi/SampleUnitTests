bca0e1bc5b4bf669a58b37e364718c65c10a79b4
Gabotto <you@example.com>
2017-06-10 22:28:05 -0300

    Commit of classes for 1.3.8 project

--- SampleUnitTests-SB_1.3.8/src/main/java/com/example/sample/unit/test/SampleUnitTestsSB_138/Application.java
+++ SampleUnitTests-SB_1.3.8/src/main/java/com/example/sample/unit/test/SampleUnitTestsSB_138/Application.java
@@ -3,9 +3,18 @@ package com.example.sample.unit.test.SampleUnitTestsSB_138;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Gabriel
+ *
+ */
 @SpringBootApplication
 public class Application {
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
